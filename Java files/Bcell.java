00242e7df270887a65695e8319d65e234d32111c
Packet-Tracer-Sysco <adeimos4812@gmail.com>
2026-04-01 21:36:15 -0600

    Created the first files, and quick basic outline

---
+++ Java files/Bcell.java
@@ -0,0 +1,10 @@
+public class Bcell {
+    public static void main(String[] args) {
+        System.out.println("Hello, I'm a B Cell!");
+    }
+    // attributes go here
+    
+    // constructor goes here
+    
+    // methods go here
+}
---
+++ Java files/Sim.java
@@ -0,0 +1,10 @@
+public class Sim {
+        public static void main(String[] args) {
+        System.out.println("Hello, I'm the simulation!");
+    }
+    // attributes go here
+    
+    // constructor goes here
+    
+    // methods go here
+}
