1035c65d524b0c1291335e16781d1667c8ece30f
Aryan-Gupta-Projects <adeimos4812@gmail.com>
2026-05-26 11:44:13 -0600

    Did little things

---
+++ Memory.java
@@ -0,0 +1,10 @@
+public class Memory {
+        public static void main(String[] args) {
+        System.out.println("Hello, I'm the memory!");
+    }
+    // attributes go here
+    
+    // constructor goes here
+    
+    // methods go here
+}
