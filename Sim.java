742a1e80b4b0ba420933edab22812a69b86972cb
Aryan-Gupta-Projects <adeimos4812@gmail.com>
2026-05-26 12:14:24 -0600

    Moved some math from Sim to VecMath

--- Sim.java
+++ Sim.java
@@ -1,19 +1,6 @@
 public class Sim {
     public static final int DIM = 10;
-    public static double simpleDot(double[] a, double[] b) {
 
-    double dot = 0;
-    double magA = 0;
-    double magB = 0;
-
-    for (int i = 0; i < a.length; i++) {
-        dot += a[i] * b[i];
-        magA += a[i] * a[i];
-        magB += b[i] * b[i];
-    }
-
-    return dot / (Math.sqrt(magA) * Math.sqrt(magB) + 1e-9);
-}
     public static void main(String[] args) {
         System.out.println("Hello, I'm the simulation!");
         //Create antigen
@@ -23,16 +10,16 @@ public class Sim {
         System.out.println(java.util.Arrays.toString(antigen.getFeatures()));
 
         //Create B Cells
-        Bcell[] bcells = new Bcell[5];
+        Bcell[] bcells = new Bcell[5]; //How many B cells wanted
         for (int i = 0; i < bcells.length; i++) {
             bcells[i] = Bcell.randomB();
         }
 
         //Temporary simple affinity (w/o engines)
         for (int i = 0; i < bcells.length; i++){
-             double score = simpleDot(
-            bcells[i].getReceptor(),
-            antigen.getFeatures()
+             double score = VecMath.cosine(
+         bcells[i].getReceptor(),
+         antigen.getFeatures()
         );
 
         bcells[i].setAffinity(score);
--- VecMath.java
+++ VecMath.java
@@ -1,10 +1,30 @@
 public class VecMath {
-        public static void main(String[] args) {
-        System.out.println("Hello, I'm the vector math!");
+
+    // cosine similarity (normalized dot product)
+    public static double cosine(double[] a, double[] b) {
+
+        double dot = 0;
+        double magA = 0;
+        double magB = 0;
+
+        for (int i = 0; i < a.length; i++) {
+            dot += a[i] * b[i];
+            magA += a[i] * a[i];
+            magB += b[i] * b[i];
+        }
+
+        return dot / (Math.sqrt(magA) * Math.sqrt(magB) + 1e-9);
     }
-    // attributes go here
-    
-    // constructor goes here
-    
-    // methods go here
+
+    // simple dot product (Optional, for testing)
+    public static double dot(double[] a, double[] b) {
+
+        double sum = 0;
+
+        for (int i = 0; i < a.length; i++) {
+            sum += a[i] * b[i];
+        }
+
+        return sum;
+    }
 }
