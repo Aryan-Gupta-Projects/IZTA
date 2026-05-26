009cd699cf61da577da2baa655b4d566db1c1fca
Aryan-Gupta-Projects <adeimos4812@gmail.com>
2026-05-26 12:51:47 -0600

    Moved a lot of stuff from Sim to others to keep Sim streamlined

--- Sim.java
+++ Sim.java
@@ -2,29 +2,10 @@ public class Sim {
     public static final int DIM = 10;
 
     public static void main(String[] args) {
-        System.out.println("Hello, I'm the simulation!");
-        //Create antigen
+
         Antigen antigen = Antigen.example();
-
-        System.out.println("Antigen:");
-        System.out.println(java.util.Arrays.toString(antigen.getFeatures()));
-
-        //Create B Cells
-        Bcell[] bcells = new Bcell[5]; //How many B cells wanted
-        for (int i = 0; i < bcells.length; i++) {
-            bcells[i] = Bcell.randomB();
-        }
-
-        //Temporary simple affinity (w/o engines)
-        for (int i = 0; i < bcells.length; i++){
-             double score = VecMath.cosine(
-         bcells[i].getReceptor(),
-         antigen.getFeatures()
-        );
-
-        bcells[i].setAffinity(score);
-
-        System.out.println("B Cell " + i + " affinity: " + score);
-        }
+        Bcell[] bcells = VecMath.randomPopulation(5);
+        VecMath.affinitybasic(bcells, antigen);
+        VecMath.printaffinity(bcells, antigen);
     }
- }
+}
--- VecMath.java
+++ VecMath.java
@@ -1,6 +1,6 @@
 public class VecMath {
 
-    // cosine similarity (normalized dot product)
+// cosine similarity (normalized dot product)
     public static double cosine(double[] a, double[] b) {
 
         double dot = 0;
@@ -16,7 +16,51 @@ public class VecMath {
         return dot / (Math.sqrt(magA) * Math.sqrt(magB) + 1e-9);
     }
 
-    // simple dot product (Optional, for testing)
+//Population Creation
+    public static Bcell[] randomPopulation(int size) {
+    Bcell[] bcells = new Bcell[size];
+
+    for (int i = 0; i < size; i++) {
+        bcells[i] = Bcell.randomB();
+    }
+
+    return bcells;
+}
+    
+//Evaluation of affinity scores
+    public static void affinitybasic(Bcell[] bcells, Antigen antigen) {
+        for (int i = 0; i < bcells.length; i++) {
+            double score = cosine(bcells[i].getReceptor(), antigen.getFeatures());
+            bcells[i].setAffinity(score);
+    }
+}
+
+//Best B cell
+public static Bcell best(Bcell[] bcells) {
+    Bcell best = bcells[0];
+    for (int i = 1; i < bcells.length; i++) {
+        if (bcells[i].getAffinity() > best.getAffinity()) {
+            best = bcells[i];
+        }
+    }
+    return best;
+}
+
+//Printing
+    public static void printaffinity(Bcell[] bcells, Antigen antigen) {
+
+    System.out.println("Antigen:");
+    System.out.println(java.util.Arrays.toString(antigen.getFeatures()));
+
+    for (int i = 0; i < bcells.length; i++) {
+        System.out.println("B Cell " + i + ": " + bcells[i].getAffinity());
+    }
+
+    Bcell best = best(bcells);
+    System.out.println("Best inital affinity: " + best.getAffinity());
+}
+
+// simple dot product (Optional, for testing)
     public static double dot(double[] a, double[] b) {
 
         double sum = 0;
