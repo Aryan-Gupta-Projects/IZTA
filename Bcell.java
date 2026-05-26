4f5ed4ff4229d73e080d92bcb26b79188d01ac01
Aryan-Gupta-Projects <adeimos4812@gmail.com>
2026-05-26 11:43:43 -0600

    More Bcell

---
+++ Bcell.java
@@ -0,0 +1,42 @@
+public class Bcell {
+
+    // attributes
+    private double[] receptor;
+    private double affinity;
+    
+    // constructor
+    public Bcell(double[] receptor) {
+        this.receptor = receptor;
+        this.affinity = 0.0;
+    }
+
+    // getter for receptor vector
+    public double[] getReceptor() {
+        return receptor;
+    }
+
+    //setter for receptor vector (for use after mutation)
+    public void setReceptor(double[] receptor) {
+        this.receptor = receptor;
+    }
+
+    //getter for affinity score
+    public double getAffinity() {
+        return affinity;
+    }
+
+    // setter for affinity score (computed during simulation)
+    public void setAffinity(double affinity) {
+        this.affinity = affinity;
+    }
+
+    // static method to generate a random BCell (initial population)
+    public static Bcell randomB(){
+        double[] r = new double[Sim.DIM]; // assuming Sim.DIM features for the receptor
+        
+        for (int i = 0; i < Sim.DIM; i++) {
+            r[i] = Math.random(); // random value between 0 and 1 for each feature
+        }
+        return new Bcell(r);
+    }
+}
