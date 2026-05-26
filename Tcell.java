4e934e6c0efc0b2911a1acb1cac0566a4516269d
Aryan-Gupta-Projects <adeimos4812@gmail.com>
2026-05-26 11:44:28 -0600

    Did little things

---
+++ Tcell.java
@@ -0,0 +1,10 @@
+public class Tcell {
+        public static void main(String[] args) {
+        System.out.println("Hello, I'm a T Cell!");
+    }
+    // attributes go here
+    
+    // constructor goes here
+    
+    // methods go here
+}
---
+++ VecMath.java
@@ -0,0 +1,10 @@
+public class VecMath {
+        public static void main(String[] args) {
+        System.out.println("Hello, I'm the vector math!");
+    }
+    // attributes go here
+    
+    // constructor goes here
+    
+    // methods go here
+}
