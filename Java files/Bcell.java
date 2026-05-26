9c0fe958bca4dcf4d17e331987184d255b1bb735
Aryan-Gupta-Projects <adeimos4812@gmail.com>
2026-05-26 10:21:51 -0600

    Spaced it better

--- Java files/Bcell.java
+++ Java files/Bcell.java
@@ -9,22 +9,27 @@ public class Bcell {
         this.receptor = receptor;
         this.affinity = 0.0;
     }
+
     // getter for receptor vector
     public double[] getReceptor() {
         return receptor;
     }
+
     //setter for receptor vector (for use after mutation)
     public void setReceptor(double[] receptor) {
         this.receptor = receptor;
     }
+
     //getter for affinity score
     public double getAffinity() {
         return affinity;
     }
+
     // setter for affinity score (computed during simulation)
     public void setAffinity(double affinity) {
         this.affinity = affinity;
     }
+
     // static method to generate a random BCell (initial population)
     public static Bcell random(int n){
         double[] r = new double[n];
@@ -34,5 +39,4 @@ public class Bcell {
         }
         return new Bcell(r);
     }
-    
 }
