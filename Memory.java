9704f9b132728d13224dff014c97b06181612bcd
adeimos4812 <adeimos4812@gmail.com>
2026-07-01 11:07:22 -0600

    feat: expand logging with deltas, mutation stats, and memory metrics

--- Memory.java
+++ Memory.java
@@ -1,10 +1,61 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Memory {
-        public static void main(String[] args) {
-        System.out.println("Hello, I'm the memory!");
+
+    private List<StoredCell> memCells;
+    private List<StoredCell> plasCells;
+
+    public List<StoredCell> getMemCells() {
+        return memCells;
     }
-    // attributes go here
-    
-    // constructor goes here
-    
-    // methods go here
+
+    public List<StoredCell> getPlasCells() {
+        return plasCells;
+    }
+
+    public int plasmaSize() {
+        return plasCells.size();
+    }
+
+    public int memorySize() {
+        return memCells.size();
+    }
+
+    public Memory() {
+        this.memCells = new ArrayList<>();
+        this.plasCells = new ArrayList<>();
+    }
+
+    public void addPlasma(Bcell cell, int iteration) {
+        plasCells.add(new StoredCell(cell, iteration, "Plasma"));
+    }
+
+    public void addMemory(Bcell cell, int iteration) {
+        memCells.add(new StoredCell(cell, iteration, "Memory"));
+    }
+
+    public static double avg(List<StoredCell> cells) {
+
+        if (cells.isEmpty()) {
+            return 0.0;
+        }
+        double sum = 0.0;
+        for (int i = 0; i < cells.size(); i++) {
+            sum += cells.get(i).getAffinity();
+        }
+        return sum / cells.size();
+    }
+
+    public void printMetaData() {
+        for (int i = 0; i < plasCells.size(); i++) {
+            System.out.println("Type: Plasma" + " | Affinity: " + plasCells.get(i).getAffinity() + " | Iteration = " + plasCells.get(i).getIteration());
+        }
+        System.out.println("------");
+        for (int i = 0; i < memCells.size(); i++) {
+            System.out.println("Type: Memory" + " | Affinity: " + memCells.get(i).getAffinity() + " | Iteration = " + memCells.get(i).getIteration());
+        }
+        
+    }
+
 }
--- Tcell.java
+++ Tcell.java
@@ -1,11 +1,26 @@
 public class Tcell {
-
-      public static Bcell selectSurvivor(Bcell parent, Bcell[] clones) {
+    
+    public static Bcell selectSurvivor(Bcell parent, Bcell[] clones) {
+        
         Bcell bestClone = AffinityEngine.best(clones);
         if (bestClone.getAffinity() > parent.getAffinity()) {
             return bestClone;
         } else {
             return parent;
         }
+    }
+    
+    public static Bcell selectPurpose(Bcell[] clones, Memory memory, int iteration) {
+        //Bcell selected = clones[StochasticEngine.getRng().nextInt(clones.length)]; Random clone selected
+        Bcell selected = AffinityEngine.best(clones); //Best clone selected
+        double affinity = selected.getAffinity();
+        if (StochasticEngine.getRng().nextDouble() < Sim.DIFFERENTIATION_CHANCE){
+            if (iteration >= Sim.PLASMA_START && iteration < Sim.MEMORY_START && affinity >= Sim.PLASMA_THRESHOLD) {
+              memory.addPlasma(selected, iteration);
+            } else if (iteration >= Sim.MEMORY_START && affinity >= Sim.MEMORY_THRESHOLD){
+              memory.addMemory(selected, iteration);
+            }
+        }
+        return selected;
     }
 }
